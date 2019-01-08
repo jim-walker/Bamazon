05de49b1c42f4b78996af2a51cf8219fb471cc6b
Walker, James E (Jim) <James.Walker1@ge.com>
2019-01-07 21:28:18 -0500

    Final working verison

--- app.js
+++ app.js
@@ -1,32 +1,52 @@
 const db = require('./models');
+const inquirer = require('inquirer');
 
 db.sequelize.sync().then(function(){
-  db.Products.findAll({
-    where: {
-      product_name: 'TomFoolery'
-    }
+  db.products.findAll({
+    where: 
+      {stock_quantity:
+        {gt: 0}
+      }
   }).then(function(data){
-    console.log('------------PRINTING DB DATA-----------------');
-    console.log(JSON.stringify(data, null, 2));
+    console.log('--------------ITEMS FOR SALE-----------------');
+    console.log(`ITEM#\t\tNAME\t\t\tPRICE`);
+    // console.log(JSON.stringify(data, null, 2));
+    for(i=0;i<data.length;i++){
+      console.log(`${data[i].item_id}\t\t${data[i].product_name}\t\t\$${data[i].price}`);
+    };
+
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'item_id',
+        message: 'Enter the ITEM# to purchase.',
+        filter: Number
+      },
+      {
+        type: 'input',
+        name: 'quantity',
+        message: 'How many such items do you require?',
+        filter: Number
+      }
+    ]).then(function (input) {
+      var itemIndex=data.map(function(e) { return e.item_id; }).indexOf(input.item_id);
+      if (input.quantity<=data[itemIndex].stock_quantity){
+        cost=data[itemIndex].price*input.quantity;
+        console.log(`Total cost of purchasing ${input.quantity} ${data[itemIndex].product_name}s is \$${cost}`);
+        data[itemIndex].stock_quantity=data[itemIndex].stock_quantity-input.quantity;
+        // console.log(`New quantity is ${data[itemIndex].stock_quantity}`);
+        db.products.update({
+          stock_quantity: data[itemIndex].stock_quantity
+        },{
+          where: { item_id: data[itemIndex].item_id }
+        }).then(function(response) {
+          console.log(`Success! Transaction complete`);
+        }).catch(function(error) {
+          console.log('Error', error);
+        });
+      } else {
+        console.log(`Sorry! Fresh Out of ${data[itemIndex].product_name}`);
+      }
+    });
   });
-
-  // db.Reservation.findAll({
-  //   where: {
-  //     phoneNumber: '444-666-3377'
-  //   }
-  // }).then(function(data){
-  //   console.log('------------PRINTING DB DATA-----------------');
-  //   console.log(JSON.stringify(data, null, 2));
-  // });
-
-  // db.Reservation.find({
-  //   where: {
-  //     phoneNumber: '444-666-3377'
-  //   }
-  // }).then(function(data){
-  //   console.log('------------PRINTING DB DATA-----------------');
-  //   console.log(JSON.stringify(data, null, 2));
-  // });
-
-
 });
--- models/Products.js
+++ models/Products.js
@@ -1,8 +1,14 @@
 module.exports = function(connection, Sequelize) {
   const Products = connection.define('products', {
+    item_id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true
+      },
     product_name: Sequelize.STRING,
     price: Sequelize.DECIMAL,
-    stock_quantity: Sequelize.INTEGER
+    stock_quantity: Sequelize.INTEGER},
+    {
+      timestamps :false
   });
 
   return Products;
