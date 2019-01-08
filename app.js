854d06820e6c70e45ec2716e3262f0fe6fd0e9a9
Walker, James E (Jim) <James.Walker1@ge.com>
2019-01-08 11:03:04 -0500

    Adding commentary

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+// Set up Dependencies
 const db = require('./models');
 const inquirer = require('inquirer');
 
+// Sync with Bamazon DB on products that are in stock
 db.sequelize.sync().then(function(){
   db.products.findAll({
     where: 
@@ -8,13 +10,14 @@ db.sequelize.sync().then(function(){
         {gt: 0}
       }
   }).then(function(data){
+    // On sync success List items for sale
     console.log('--------------ITEMS FOR SALE-----------------');
     console.log(`ITEM#\t\tNAME\t\t\tPRICE`);
     // console.log(JSON.stringify(data, null, 2));
     for(i=0;i<data.length;i++){
       console.log(`${data[i].item_id}\t\t${data[i].product_name}\t\t\$${data[i].price}`);
     };
-
+    // Prompt User for Item and Quantity
     inquirer.prompt([
       {
         type: 'input',
@@ -29,12 +32,17 @@ db.sequelize.sync().then(function(){
         filter: Number
       }
     ]).then(function (input) {
+      // On prompt success get Index of Item in data Array
       var itemIndex=data.map(function(e) { return e.item_id; }).indexOf(input.item_id);
+      // Evaluate if in stock quantity is sufficient for order
       if (input.quantity<=data[itemIndex].stock_quantity){
+        // If stock is sufficient message total cost to User
         cost=data[itemIndex].price*input.quantity;
-        console.log(`Total cost of purchasing ${input.quantity} ${data[itemIndex].product_name}s is \$${cost}`);
+        console.log(`Total cost of purchasing ${input.quantity} ${data[itemIndex].product_name}s is \$${cost.toFixed(2)
+        }`);
+        // Subtract order from quantity in data array
         data[itemIndex].stock_quantity=data[itemIndex].stock_quantity-input.quantity;
-        // console.log(`New quantity is ${data[itemIndex].stock_quantity}`);
+        // Update database with new quantity
         db.products.update({
           stock_quantity: data[itemIndex].stock_quantity
         },{
@@ -45,6 +53,7 @@ db.sequelize.sync().then(function(){
           console.log('Error', error);
         });
       } else {
+        // If stock is not sufficient inform the user of their incredible bad fortune
         console.log(`Sorry! Fresh Out of ${data[itemIndex].product_name}`);
       }
     });
